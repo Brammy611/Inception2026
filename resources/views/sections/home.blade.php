f3334bf31d298e6b76bc6780b874cfa1e914361b
Agathan Khairy <agathan027@gmail.com>
2025-12-28 00:19:32 +0700

    tes

--- resources/views/sections/home.blade.php
+++ resources/views/sections/home.blade.php
@@ -1,11 +1,11 @@
 <section class="home" id="home">
   {{-- Animated Blur Circles Background --}}
-  <div class="blur-circle"></div>
-  <div class="blur-circle"></div>
-  <div class="blur-circle"></div>
-  <div class="blur-circle"></div>
-  <div class="blur-circle"></div>
-  <div class="blur-circle"></div>
+  <!-- <div class="blur-circle"></div> -->
+  <!-- <div class="blur-circle"></div> -->
+  <!-- <div class="blur-circle"></div> -->
+  <!-- <div class="blur-circle"></div> -->
+  <!-- <div class="blur-circle"></div> -->
+  <!-- <div class="blur-circle"></div> -->
 
   {{-- <div class="logo-3d">
     <spline-viewer url="https://prod.spline.design/ONMSJVwBqdCXOk-v/scene.splinecode"></spline-viewer>
